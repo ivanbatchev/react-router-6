01a9bdc4dc4fade12b6a0cdd2c8f63d5709d6f74
Ivan Batchev <batchev.ivan@yandex.ru>
2024-11-05 01:25:06 +0300

    5-nested-routes-add: add layout component for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Header } from './shared/components'
 import { Home, About, Vans, VanDetails } from './pages'
 import './App.css'
+import { Layout } from './shared/components'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetails />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Loader from "./Loader";
 import VanCard from "./VanCard";
+import Layout from "./Layout";
 
 export {
   Header,
   Loader,
-  VanCard
+  VanCard,
+  Layout
 }
